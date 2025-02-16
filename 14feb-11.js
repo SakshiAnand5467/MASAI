51fc18b6fd0f0bf928120264c46a80963835239e
SakshiAnand5467 <102008901+SakshiAnand5467@users.noreply.github.com>
2025-02-16 23:36:51 +0530

    Create 14feb-11.js

---
+++ 14feb-11.js
@@ -0,0 +1,10 @@
+function outerFunction() {
+    let message = "Hello World!";
+    
+    return function innerFunction() {
+      console.log(message);
+    };
+  }
+
+  const c = outerFunction(); 
+  c();
